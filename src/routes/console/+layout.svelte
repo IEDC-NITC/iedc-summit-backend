4bda6035ea80addd76ffd58bde440a786836c637
Nirmal Thomas Mathew <55346160+nirmal15mathew@users.noreply.github.com>
2024-07-25 22:12:13 +0530

    minor fix

--- src/routes/console/+layout.svelte
+++ src/routes/console/+layout.svelte
@@ -107,7 +107,7 @@
               >Lectures</a
             >
             <a
-                href="/console/lectures"
+                href="/console/workshops"
                 class={$page.url.pathname == "/console/workshops"
                   ? "block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white"
                   : "block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"}
