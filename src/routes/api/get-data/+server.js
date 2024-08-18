a5e19e6f9c50aa1095ef78106b812e9ff13f98a3
th3bossc <diljith2003@gmail.com>
2024-08-18 15:19:55 +0530

    updated image url

--- src/routes/api/get-data/+server.js
+++ src/routes/api/get-data/+server.js
@@ -1,9 +1,28 @@
-import { getAllEvents, getAllLectures, getAllWorkshops } from "$lib/firebase-setup";
-
+import { getAllEvents, getAllLectures, getAllWorkshops, storage } from "$lib/firebase-setup";
+import { getDownloadURL, ref } from "firebase/storage"
 export async function GET() {
-    const events = await getAllEvents();
-    const lectures = await getAllLectures();
-    const workshops = await getAllWorkshops();
+    const events = (await getAllEvents()).map(async (event) => {
+        const imgUrl = await getDownloadURL(ref(storage, event.posterImage)),
+        return {
+            ...event,
+            imgUrl,
+        }
+    })
+
+    const lectures = (await getAllLectures()).map(async (lecture) => {
+        const imgUrl = await getDownloadURL(ref(storage, lecture.posterImage)),
+        return {
+            ...lecture,
+            imgUrl,
+        }
+    })
+    const workshops = (await getAllWorkshops()).map(async (workshop) => {
+        const imgUrl = await getDownloadURL(ref(storage, workshop.posterImage)),
+        return {
+            ...workshop,
+            imgUrl,
+        }
+    })
     return new Response(JSON.stringify({ events, lectures, workshops }), {
         headers: {
             'Content-Type': 'application/json',
--- src/routes/getEvents/+server.js
+++ src/routes/getEvents/+server.js
@@ -4,10 +4,9 @@ import { getDownloadURL, ref } from "firebase/storage"
 
 export async function GET() {
     let events = await getAllEvents()
-    let new_events = []
     for (let event of events) {
         const imgUrl = await getDownloadURL(ref(storage, event.posterImage))
-        new_events.push({...event, imgUrl})
+        new_events.push({ ...event, imgUrl })
     }
 
 
