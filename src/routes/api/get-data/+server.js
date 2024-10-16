61eebcfb1e9776e6b6e14cb49710aab3d968ce63
aliasgeorge9329 <aliasgeorge9329@gmailcom>
2024-10-16 10:58:53 +0530

    update

--- src/routes/api/get-data/+server.js
+++ src/routes/api/get-data/+server.js
@@ -1,10 +1,32 @@
 import { getAllDataFormatted, storage } from "$lib/firebase-setup";
+import axios from "axios";
 import { getDownloadURL, ref } from "firebase/storage";
 export async function GET() {
   let events = await getAllDataFormatted("events");
   let lectures = await getAllDataFormatted("lectures");
   let workshops = await getAllDataFormatted("workshops");
-  let speakers = await getAllDataFormatted("speakers");
+  let speakers = await axios.get("https://events.startupmission.in/api/event/iedcsummit-2024/speakers")
+  let data = speakers.data['Kerala Startup Mission']
+
+  data.forEach(( /** @type {{ [x: string]: any; }} */ obj) => {
+    obj["Name"] = obj['name'];
+    obj["Company"] = obj['organisation'];
+    obj["Image"] = obj['photo'];
+    obj["Designation"] = obj['designation'];
+    obj["LinkedIn"] = obj['linkedin'];
+    obj["Priority"] = obj['order'];
+    obj["imgUrl"] = obj['photo'];
+
+    delete obj['name'];
+    delete obj['organisation'];
+    delete obj['photo'];
+    delete obj['designation'];
+    delete obj['linkedin'];
+    delete obj['order'];
+    delete obj['photo'];
+    delete obj['external_id']
+  });
+
   let partners = await getAllDataFormatted("partners");
   let news = await getAllDataFormatted("news");
   let faqs = await getAllDataFormatted("faqs");
@@ -15,7 +37,7 @@ export async function GET() {
       lectures,
       workshops,
       news,
-      speakers,
+      speakers: data,
       partners,
       faqs,
     }),
